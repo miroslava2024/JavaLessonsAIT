62bad1f11525585263867dd047076d1686938011
miroslava2024 <160175824+miroslava2024@users.noreply.github.com>
2024-02-27 22:52:17 +0100

    Palindrom Lesson12

---
+++ src/java/lesson12/Palindrom.java
@@ -0,0 +1,19 @@
+package lesson12;
+
+public class Palindrom {
+    public static void main(String[] args) {
+        // mom, ded, poop, race car, nurses run
+
+       String s = "rase car";
+       System.out.println(isPalindrome(s) ? "yes" : "no");
+
+    }
+    public static boolean isPalindrome(String s) {
+        if (s.length() == 1 || s.length() == 0) {
+            return true;
+        }
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            return isPalindrome(s.substring(1, s.length() - 1));
+        }return false;
+    }
+}
