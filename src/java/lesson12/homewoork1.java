9e8cc2a4c52b56e2e5a1bd5f72ef5465d3b9bf09
miroslava2024 <160175824+miroslava2024@users.noreply.github.com>
2024-03-05 18:30:00 +0100

    Palindrom Lesson12

--- src/java/lesson12/homewoork1.java
+++ src/java/lesson12/homewoork1.java
@@ -3,22 +3,35 @@ package lesson12;
 import java.util.Scanner;
 
 public class homewoork1 {
-    Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        double a = 5;
-        double b = 4;
-        calkulateArea(a,b);
-        calkulatePerimeter(a,b);
-
-    }
-    public static void calkulateArea(double a, double b){
-        double multi = a * b;
-        System.out.println("Площядь прямоугольника " + multi);
-
-
-    }
-    public static void calkulatePerimeter(double a, double b){
-        double perimeter = 2 * a + b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" length --> ");
+        double a = scanner.nextDouble();
+        System.out.println("weigth --> ");
+        double b = scanner.nextDouble();
+        double area = calkulateArea(a,b);
+        double perimeter = calkulatePerimeter(a,b);
+        System.out.println("Площядь прямоугольника " + area);
         System.out.println( "Периметер прямоугольника " + perimeter);
+    }
+    public static double calkulateArea(double a, double b){
+        if(a >= 0 || b <= 0){
+            System.out.println("ERROR");
+            return -1;
+        }
+        return a * b;
+
+
+
+    }
+    public static double calkulatePerimeter(double a, double b){
+        if(a >= 0 || b <= 0){
+            System.out.println("ERROR");
+            return -1;
+        }
+        return 2 *(a + b);
+
     }
 }
